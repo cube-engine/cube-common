ebe1fda798d6a1bc701f4185f33ab79a66e124b2
liubing <liubing@mobike.com>
2022-05-15 22:31:56 +0800

    to #2

---
+++ src/main/java/net/cube/engine/Constant.java
@@ -0,0 +1,15 @@
+package net.cube.engine;
+
+/**
+ * @author pluto
+ * @date 2022/5/15
+ */
+public interface Constant {
+
+    String DOT = "\\.";
+
+    String OB = "[";
+
+    String CB = "]";
+
+}
